59e711a98f35668836ecb33d18ebb64e163ccb4a
daniel <506682730@qq.com>
2019-01-15 17:26:22 +0800

    Signed-off-by: daniel <506682730@qq.com>

---
+++ data/conf/extra/webconfig.php
@@ -0,0 +1,1 @@
+<?php return ['admin_title'=>'内容管理平台','title'=>'内容管理平台','keywords'=>'内容管理平台','logo'=>'\upload\admin\thumb\1d\928df7298f59c0fb1829db44537cfaac71a3d5.jpg','desc'=>'内容管理平台','icp'=>'','code'=>'','domain'=>'','port_cache_time'=>''];
