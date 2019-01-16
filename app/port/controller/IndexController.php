38c8381b445ad3b0d8047f1de966b186be98bb07
daniel <506682730@qq.com>
2019-01-16 14:17:07 +0800

    Signed-off-by: daniel <506682730@qq.com>

--- app/port/controller/IndexController.php
+++ app/port/controller/IndexController.php
@@ -16,9 +16,9 @@ class IndexController extends PortControllerBase
    public function index()
    {
        return $this->port(1,'请求成功',[
-       		'name'   => '我叫vaeThink 这是我的第一个api接口',
-       		'domain' => '我的唯一官网：www.vaeThink.com',
+       		'name'   => '后台登录网址',
+       		'domain' => '我的唯一官网：http://cms.mankkk.cn/admin/publicer/login.html',
        		'desc'   => '如果您在使用中遇到疑问，请前往官网查看开发者文档，或通过官网去到开发者社区寻求帮助。'
-       ],'http://www.vaethink.com');
+       ],'');
    }
 }
